1104c62d205cc3312547bad271a14089c349acf4
danax7 <dan.praeco@gmail.com>
2023-05-02 23:16:00 +0700

    added console button

--- app/src/main/java/com/example/mobile/MainActivity.kt
+++ app/src/main/java/com/example/mobile/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,6 +18,7 @@ import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.example.mobile.Blocks.Initialization
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
@@ -30,53 +33,95 @@ var height = 0.dp
 var width = 0.dp
 
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun OnLoad() {
-    Column() {
-        var countBlocks by remember { mutableStateOf (0) }
-        InitBlock(variables = variables)
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.9f)
-                .background(Color.Gray)
-                .onGloballyPositioned { coordinates ->
-                    // Set column height using the LayoutCoordinates
-                    height = with(LocalDensity) { (coordinates.size.height * 0.9).dp }
-                    width = with(LocalDensity) { coordinates.size.width.dp }
+    val sheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+    val coroutineScope = rememberCoroutineScope()
+    ModalBottomSheetLayout(
+        sheetState = sheetState,
+        sheetContent = { ModalContent() }
+    ) {
+        Column() {
+            var countBlocks by remember { mutableStateOf (0) }
+            InitBlock(variables = variables)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.9f)
+                    .background(Color.Gray)
+                    .onGloballyPositioned { coordinates ->
+                        // Set column height using the LayoutCoordinates
+                        height = with(LocalDensity) { (coordinates.size.height * 0.9).dp }
+                        width = with(LocalDensity) { coordinates.size.width.dp }
+                    }
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    Spacer(Modifier.weight(1f))
+                    FloatingActionButton(
+                        onClick = {
+                            // Отобразить модальное окно
+                            coroutineScope.launch {
+                                sheetState.show()
+                            }
+                        },
+                        shape = MaterialTheme.shapes.medium,
+                        backgroundColor = MaterialTheme.colors.primary,
+                        contentColor = MaterialTheme.colors.onPrimary
+                    ) {
+                        Text(text = "Консоль")
+                    }
                 }
 
-        )
-
-        {
-            repeat(countBlocks)
-            {
-                MakeBlock()
+                repeat(countBlocks) {
+                    MakeBlock()
+                }
             }
-        }
 
-        Box(
-            modifier = Modifier
-                .fillMaxSize(),
-            contentAlignment = Alignment.BottomEnd
-        )
-
-        {
-            Button(onClick = {
-                countBlocks++
-                println("new block created")
-            },
+            Box(
                 modifier = Modifier
-                    .padding(15.dp)
-                    .size(60.dp, 60.dp)
+                    .fillMaxSize(),
+                contentAlignment = Alignment.BottomEnd
             )
+
             {
-                Text(text = "+")
+
+                Button(onClick = {
+                    countBlocks++
+                    println("new block created")
+                },
+                    modifier = Modifier
+                        .padding(15.dp)
+                        .size(60.dp, 60.dp)
+                )
+                {
+                    Text(text = "+")
+                }
+
             }
         }
+    }
 
+}
+
+@Composable
+fun ModalContent() {
+    var text by remember { mutableStateOf("") }
+    Column(modifier = Modifier
+        .padding(20.dp)
+        .height(40.dp)
+    ) {
+        Text(text = "Вывод")
     }
 }
+
+
+
+
 
 @Composable
 fun ButtonAdd()
@@ -94,7 +139,6 @@ fun ButtonAdd()
     }
 }
 
-//data class Variable(val name: String, var value: Int = 0)
 var variables = mutableMapOf<String, String>()
 
 @Composable
