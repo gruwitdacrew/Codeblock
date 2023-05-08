67e624360eee2093f9abbbcc8f36795f5520914f
danax7 <dan.praeco@gmail.com>
2023-05-08 15:46:47 +0700

    added visualization of delete block button

--- app/src/main/java/com/example/mobile/PrintBlock.kt
+++ app/src/main/java/com/example/mobile/PrintBlock.kt
@@ -1,12 +1,18 @@
 package com.example.mobile
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,7 +24,10 @@ import androidx.compose.ui.unit.sp
 
 
 @Composable
-fun PrintBlock(String: String) {
+fun PrintBlock(
+    string: String,
+//    onDelete: () -> Unit
+) {
     var text by remember { mutableStateOf("") }
 
     Card(
@@ -28,20 +37,33 @@ fun PrintBlock(String: String) {
         shape = RoundedCornerShape(16.dp),
         backgroundColor = Color.LightGray
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically
+
+        Column(
+            modifier = Modifier.padding(8.dp)
         ) {
-            Text("Print", modifier = Modifier.padding(14.dp))
-            TextField(
-                value = text,
-                onValueChange = { newText ->
-                    text = newText
-                },
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(16.dp)
-                    .background(Color.Transparent)
-            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Print", modifier = Modifier.padding(14.dp))
+                TextField(
+                    value = text,
+                    onValueChange = { newText ->
+                        text = newText
+                    },
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(end = 16.dp)
+                        .background(Color.Transparent)
+                )
+                IconButton(
+                    onClick = { /*onDelete*/ },
+                    modifier = Modifier.padding(end = 8.dp)
+                ) {
+                    Icon(Icons.Default.Delete, contentDescription = "delete")
+                }
+
+            }
+
         }
     }
 }
