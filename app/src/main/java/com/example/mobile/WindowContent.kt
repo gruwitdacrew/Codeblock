9c5216ef4f7ce9a55640fb11d0a9037312e0dd96
danax7 <dan.praeco@gmail.com>
2023-05-11 11:48:46 +0700

    Structure fix

--- app/src/main/java/com/example/mobile/WindowContent.kt
+++ app/src/main/java/com/example/mobile/WindowContent.kt
@@ -14,49 +14,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.mobile.Utils.start
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-
-fun start(item:String, lines:MutableList<String>){
-    when(item[0]){
-        '=' -> {
-            val taskNow = item.substring(1).split("=", limit = 2);
-            variables[taskNow[0]] = RPS.fromRPS(variables, RPS.toRPS(taskNow[1]))
-        }
-        '?' -> {
-            var taskNow = item.substring(1).split(":", limit=2).toMutableList()
-            val indexOfElse = Regex("-?[0-9]+").find(taskNow[0])!!.value.toInt()
-            val condition = Regex("-?[0-9]+").replaceFirst(taskNow[0], "")
-            if(indexOfElse != -1){
-                val ifActions = taskNow[1].substring(0,indexOfElse-1).split(",");
-                val elseActions = taskNow[1].substring(indexOfElse).split(",")
-                println("${ifActions} ${elseActions}")
-                if(RPS.calculate(variables, condition) == "1"){
-                    ifActions.forEach { action ->
-                        start(action,lines)
-                    }
-                }
-                else{
-                    elseActions.forEach { action ->
-                        start(action,lines)
-                    }
-                }
-            }
-            else{
-                if(RPS.calculate(variables, condition) == "1"){
-                    val ifActions = taskNow[1].split(",")
-                    ifActions.forEach { action ->
-                        start(action,lines)
-                    }
-                }
-            }
-        }
-        '/' -> {
-            val taskNow = item.substring(1)
-            lines.add(RPS.calculate(variables,taskNow))
-        }
-    }
-}
 
 
 @OptIn(ExperimentalMaterialApi::class)
