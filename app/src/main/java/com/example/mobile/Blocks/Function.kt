e67114ce9556b5b85aa48dfee82a145157f7a6d0
gruwitdacrew <arsen190104@gmail.com>
2023-05-27 12:00:57 +0700

    final^4

--- app/src/main/java/com/example/mobile/Blocks/Function.kt
+++ app/src/main/java/com/example/mobile/Blocks/Function.kt
@@ -1,9 +1,5 @@
 package com.example.mobile.Blocks
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -37,8 +33,28 @@ import androidx.compose.ui.text.font.FontFamily
 import com.example.mobile.*
 import com.example.mobile.R
 import com.example.mobile.Utils.BlockInformation
-import com.example.mobile.Utils.getExpression
-import com.example.mobile.ui.theme.*
+import com.example.mobile.ui.theme.condition_color_1
+import com.example.mobile.ui.theme.condition_color_2
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+fun getFunctionExpression(
+    name:String,
+    type: String,
+    functionBlocks:MutableList<Block>,
+    argumentBlocks:MutableList<Block>,
+):String{
+    var actions = mutableListOf<String>(); var arguments = mutableListOf<String>()
+    for(i in functionBlocks){
+        actions.add(i.expression.value)
+    }
+    for(i in argumentBlocks){
+        arguments.add(i.expression.value)
+    }
+    if(actions.size > 0 ) return "*${type};${name};${Json.encodeToString(arguments)}:${Json.encodeToString(actions)}"
+    else return "";
+}
+
 
 @Composable
 fun FunctionBlock(
@@ -62,17 +78,16 @@ fun FunctionBlock(
 
     for(i in functionBlocksToRender){
         LaunchedEffect(i.expression.value){
-            blocks[index].expression.value = getExpression(name,type, functionBlocksToRender, argumentsToRender)
+            blocks[index].expression.value = getFunctionExpression(name,type, functionBlocksToRender, argumentsToRender)
             println(blocks[index].expression.value)
         }
     }
     for(i in argumentsToRender){
         LaunchedEffect(i.expression.value){
-            blocks[index].expression.value = getExpression(name,type, functionBlocksToRender, argumentsToRender)
+            blocks[index].expression.value = getFunctionExpression(name,type, functionBlocksToRender, argumentsToRender)
             println(blocks[index].expression.value)
         }
     }
-
 
     BlockSample(view = view, shape = RoundedCornerShape(5), inside =
     {
@@ -80,7 +95,7 @@ fun FunctionBlock(
             modifier = Modifier
                 .background(
                     brush = Brush.linearGradient(
-                        colors = listOf(func_color_1, func_color_2))
+                        colors = listOf(condition_color_1, condition_color_2))
                 ),
             verticalArrangement = Arrangement.SpaceBetween
         ){
@@ -89,158 +104,18 @@ fun FunctionBlock(
                     .fillMaxWidth()
                     .border(
                         width = 2.dp, color = Color.Black, shape = RectangleShape
-                    ),
-                verticalAlignment = Alignment.CenterVertically,
-
+                    )
+                    .padding(start = 20.dp),
+                verticalAlignment = Alignment.CenterVertically
             )
             {
-                Box(
-                    modifier = Modifier
-                        .width(110.dp)
-                        .fillMaxHeight()
-                )
-                {
-                    var expanded by remember { mutableStateOf(false) }
-                    DropdownMenu(
-                        modifier = Modifier
-                            .background(color = Color.Black),
-                        expanded = expanded,
-                        onDismissRequest = { expanded = false },
-                    )
-                    {
-                        DropdownMenuItem(
-                            onClick = {
-                                type = "Void"
-                                expanded = false
-                                name = name.trim()
-                                blocks[index].expression.value = getExpression(name,type, functionBlocksToRender, argumentsToRender)
-                            }
-                        ) {
-                            Text(
-                                "Void",
-                                color = Color.White,
-                                fontFamily = FontFamily(Font(R.font.fedra_sans)),
-                                fontSize = 15.sp,
-                                textAlign = TextAlign.Center
-                            )
-                        }
-                        DropdownMenuItem(
-                            onClick = {
-                                type = "Int"
-                                expanded = false
-                                name = name.trim()
-                                blocks[index].expression.value = getExpression(name,type, functionBlocksToRender, argumentsToRender)
-                            }
-                        ) {
-                            Text(
-                                text = "Int",
-                                color = Color.White,
-                                fontFamily = FontFamily(Font(R.font.fedra_sans)),
-                                fontSize = 15.sp,
-                                textAlign = TextAlign.Center
-                            )
-                        }
-                        DropdownMenuItem(
-                            onClick = {
-                                type = "String"
-                                expanded = false
-                                name = name.trim()
-                                blocks[index].expression.value = getExpression(name,type, functionBlocksToRender, argumentsToRender)
-                            }
-                        ) {
-                            Text(
-                                text = "String",
-                                color = Color.White,
-                                fontFamily = FontFamily(Font(R.font.fedra_sans)),
-                                fontSize = 15.sp,
-                                textAlign = TextAlign.Center
-                            )
-                        }
-                        DropdownMenuItem(
-                            onClick = {
-                                type = "Bool"
-                                expanded = false
-                                name = name.trim()
-                                blocks[index].expression.value = getExpression(name,type, functionBlocksToRender, argumentsToRender)
-                            }
-                        ) {
-                            Text(
-                                "Bool",
-                                color = Color.White,
-                                fontFamily = FontFamily(Font(R.font.fedra_sans)),
-                                fontSize = 15.sp,
-                                textAlign = TextAlign.Center
-                            )
-                        }
-                        DropdownMenuItem(
-                            onClick = {
-                                type = "Array\n<Int>"
-                                expanded = false
-                                name = name.trim()
-                                blocks[index].expression.value = getExpression(name,type, functionBlocksToRender, argumentsToRender)
-                            }
-                        ) {
-                            Text(
-                                "Array\n<Int>",
-                                color = Color.White,
-                                fontFamily = FontFamily(Font(R.font.fedra_sans)),
-                                fontSize = 15.sp,
-                                textAlign = TextAlign.Center
-                            )
-                        }
-                        DropdownMenuItem(
-                            onClick = {
-                                type = "Array\n<String>"
-                                expanded = false
-                                name = name.trim()
-                                blocks[index].expression.value = getExpression(name,type, functionBlocksToRender, argumentsToRender)
-                            }
-                        ) {
-                            Text(
-                                "Array\n<String>",
-                                color = Color.White,
-                                fontFamily = FontFamily(Font(R.font.fedra_sans)),
-                                fontSize = 15.sp,
-                                textAlign = TextAlign.Center
-                            )
-                        }
-                        DropdownMenuItem(
-                            onClick = {
-                                type = "Array\n<Bool>"
-                                expanded = false
-                                name = name.trim()
-                                blocks[index].expression.value = getExpression(name,type, functionBlocksToRender, argumentsToRender)
-                            }
-                        ) {
-                            Text(
-                                "Array\n<Bool>",
-                                color = Color.White,
-                                fontFamily = FontFamily(Font(R.font.fedra_sans)),
-                                fontSize = 15.sp,
-                                textAlign = TextAlign.Center
-                            )
-                        }
-                    }
-                    Button(
-                        onClick = { expanded = true },
-                        colors = ButtonDefaults.buttonColors(func_color_2),
-                        modifier = Modifier
-                            .defaultMinSize(minHeight = 70.dp)
-                            .fillMaxSize()
-                    )
-                    {
-                        Text(
-                            text = type.ifBlank { "Select\nType" },
-                            color = Color.White,
-                            fontFamily = FontFamily(Font(R.font.fedra_sans)),
-                            fontSize = 15.sp,
-                            textAlign = TextAlign.Center
-                        )
-                    }
-                }
+                TextFieldSample(modifier = Modifier.weight(2f), onValueChange = {newText ->
+                    type = newText
+                    blocks[index].expression.value = getFunctionExpression(name,type, functionBlocksToRender, argumentsToRender)
+                })
                 Text(
                     modifier = Modifier
-                        .padding(10.dp),
+                        .padding(end = 20.dp),
                     text = "function",
                     fontSize = 30.sp,
                     color = Color.White,
@@ -249,52 +124,22 @@ fun FunctionBlock(
                 )
                 TextFieldSample(modifier = Modifier.weight(2f), onValueChange = {newText ->
                     name = newText
-                    blocks[index].expression.value = getExpression(name,type, functionBlocksToRender, argumentsToRender)
+                    blocks[index].expression.value = getFunctionExpression(name,type, functionBlocksToRender, argumentsToRender)
                 })
             }
             Column(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
-            )
-            {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 30.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                )
-                {
-                    Text(
-                        text = "on input",
-                        fontSize = 30.sp,
-                        color = Color.White,
-                        fontFamily = FontFamily(Font(R.font.fedra_sans)),
-                        textAlign = TextAlign.Center
-                    )
-                }
-                for (item in argumentsToRender) {
-                    key(item) {
-                        if (!item.visibleState.currentState && !item.visibleState.targetState) handleBlockDelete(
-                            item.id,
-                            argumentsToRender
-                        )
-                        AnimatedVisibility(
-                            visibleState = item.visibleState,
-                            enter = scaleIn(animationSpec = tween(durationMillis = 100)),
-                            exit = scaleOut(animationSpec = tween(durationMillis = 100)),
-                        )
-                        {
-                            item.element()
-                        }
-                        println("${item.id} ${item.offset}")
-                    }
+            ) {
+                argumentsToRender.forEach{block ->
+                    block.element()
                 }
                 Button(
                     modifier = Modifier
                         .size(60.dp, 35.dp),
                     onClick = {
-                        chooseNow.value  = "args"
+                        chooseNow.value = view.namesOfParentsBlocks + " args"
                         blocksToAdd = argumentsToRender
                         scope.launch{drawerState.open()}
                     },
@@ -304,54 +149,25 @@ fun FunctionBlock(
                     Image(painter = painterResource(id = R.drawable.add), contentDescription = null, contentScale = ContentScale.Fit)
                 }
             }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .border(
-                        width = 2.dp, color = Color.Black, shape = RectangleShape
-                    )
-                    .padding(horizontal = 30.dp),
-                verticalAlignment = Alignment.CenterVertically
+            Text(
+                text = "actions",
+                textAlign = TextAlign.Center
             )
-            {
-                Text(
-                    text = "do",
-                    fontSize = 30.sp,
-                    color = Color.White,
-                    fontFamily = FontFamily(Font(R.font.fedra_sans)),
-                    textAlign = TextAlign.Center
-                )
-            }
             Column(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
-            )
-            {
-                for (item in functionBlocksToRender) {
-                    key(item) {
-                        if (!item.visibleState.currentState && !item.visibleState.targetState) handleBlockDelete(
-                            item.id,
-                            functionBlocksToRender
-                        )
-                        AnimatedVisibility(
-                            visibleState = item.visibleState,
-                            enter = scaleIn(animationSpec = tween(durationMillis = 100)),
-                            exit = scaleOut(animationSpec = tween(durationMillis = 100)),
-                        )
-                        {
-                            item.element()
-                        }
-                        println("${item.id} ${item.offset}")
-                    }
+            ) {
+                functionBlocksToRender.forEach{block ->
+                    block.element()
                 }
                 Button(
                     modifier = Modifier
                         .size(60.dp, 35.dp),
                     onClick = {
-                        chooseNow.value  = "function"
+                        chooseNow.value = view.namesOfParentsBlocks + " function"
                         blocksToAdd = functionBlocksToRender
-                        scope.launch{ drawerState.open() }
+                        scope.launch{drawerState.open()}
                     },
                     colors = ButtonDefaults.buttonColors(Color.Transparent),
                 )
@@ -361,11 +177,10 @@ fun FunctionBlock(
             }
             IconButton(
                 onClick = {
-                    blocks[index].visibleState.targetState = false
+                    handleBlockDelete(view.id, blocks)
                 },
                 modifier = Modifier.padding(start = 8.dp)
-            )
-            {
+            ) {
                 Icon(Icons.Default.Delete, contentDescription = "delete", tint = Color.White)
             }
         }
